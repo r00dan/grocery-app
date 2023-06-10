b7b7ddb43afdf5396639ccfdd4ae359961accd0c
Bogdan <rudenko.rbi@gmail.com>
2023-06-10 13:49:32 +0200

    added react query dependency and provider

--- frontend/components/index.ts
+++ frontend/components/index.ts
@@ -3,3 +3,4 @@ export { SearchInput } from './SearchInput/SearchInput';
 export { Product } from './Product/Product';
 export { ProductList } from './ProductList/ProductList';
 export { Component as ThemeProvider } from './ThemeProvider/ThemeProvider';
+export { ReactQueryProvider } from './ReactQueryProvider/ReactQueryProvider';
