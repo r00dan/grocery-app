303691c23b6758cda94c1c288c4c30efda66c61d
Bogdan <rudenko.rbi@gmail.com>
2023-06-12 16:53:46 +0200

    fixed docker issues and changed DB

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -5,8 +5,14 @@ import { Product } from './entities';
 const entities = [Product];
 
 export const database = new DataSource({
-  type: 'sqlite',
-  database: 'grocery-db',
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT ?? 5432),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  migrationsTableName: 'migration',
+  migrations: ['src/migration/*.ts'],
   synchronize: true,
   entities,
 });
